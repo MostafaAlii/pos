341df64453d82df71e010cf03544551f699aff63
MostafaAlii <mostafa0alii@gmail.com>
2022-09-27 07:05:31 +0200

    finish user & admin login and redirect after login

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -1,25 +1,13 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Http\Request;
 use Laravel\Fortify\Fortify;
-use App\Actions\Fortify\CreateNewUser;
-use Illuminate\Support\Facades\Config;
+use App\Actions\Fortify\{ResetUserPassword, UpdateUserPassword, UpdateUserProfileInformation, CreateNewUser};
+use Illuminate\Support\Facades\{Config, RateLimiter};
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Cache\RateLimiting\Limit;
-use App\Actions\Fortify\ResetUserPassword;
-use App\Actions\Fortify\UpdateUserPassword;
-use Illuminate\Support\Facades\RateLimiter;
-use App\Actions\Fortify\UpdateUserProfileInformation;
-
-class FortifyServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
+use Laravel\Fortify\Contracts\LoginResponse;
+class FortifyServiceProvider extends ServiceProvider {
     public function register() {
         $request = request();
         if($request->is('admin/*')) {
@@ -27,30 +15,27 @@ class FortifyServiceProvider extends ServiceProvider
             Config::set('fortify.passwords', 'admins');
             Config::set('fortify.prefix', 'admin');
         }
+        $this->app->instance(LoginResponse::class, new class implements LoginResponse {
+            public function toResponse($request) {
+                if ($request->user('admin')) 
+                    return redirect()->intended(RouteServiceProvider::ADMIN_DASHBOARD);
+                return redirect()->intended(RouteServiceProvider::HOME);
+            }
+        });
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
+    public function boot() {
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
-
         RateLimiter::for('login', function (Request $request) {
             $email = (string) $request->email;
-
             return Limit::perMinute(5)->by($email.$request->ip());
         });
-
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
         });
-
         Fortify::viewPrefix(Config::get('fortify.guard') == 'admin' ? 'dashboard.auth.admin.' : 'dashboard.auth.user.');
     }
 }
